0dfc799c0167873d9c96eae0f834f31fddacfe2e
wurui <1009479218@qq.com>
2022-05-27 21:43:31 +0800

    Update server.go

--- sqs/server.go
+++ sqs/server.go
@@ -182,6 +182,7 @@ func (srv *Server) handleMessage(ctx context.Context, message types.Message) err
 		} else {
 			return err
 		}
+		return nil
 	}
 
 	if _, err := srv.client.DeleteMessage(ctx, &sqs.DeleteMessageInput{
